86d9d40090c17f402559811cccdf63d4fdbcec75
trentspivey <141067549+trentspivey@users.noreply.github.com>
2025-02-11 10:45:43 -0500

    Load MisterRogers.splat
    Changed line 749 from 
    params.get("url") || "train.splat",
    to
    params.get("url") || "MisterRogers.splat",
    
    Changed line 750 from 
    "https://huggingface.co/cakewalk/splat-data/resolve/main/",
    to
    "https://drive.google.com/uc?id=1HrcDUL8_z4_ZeifAHAWFeUgvBRDT67aV"

--- main.js
+++ main.js
@@ -746,8 +746,8 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "train.splat",
-        "https://huggingface.co/cakewalk/splat-data/resolve/main/",
+        params.get("url") || "MisterRogers.splat",
+        "https://drive.google.com/uc?id=1HrcDUL8_z4_ZeifAHAWFeUgvBRDT67aV",
     );
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
