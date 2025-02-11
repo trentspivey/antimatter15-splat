a80671f7df5ef6f734f46b6733e4927aeefe1326
trentspivey <141067549+trentspivey@users.noreply.github.com>
2025-02-11 09:08:13 -0500

    Update main.js
    Commented out line 1483:
    
        // document.getElementById("spinner").style.display = "none";

--- main.js
+++ main.js
@@ -1480,6 +1480,6 @@ async function main() {
 
 main().catch((err) => {
     console.error(err);
-    document.getElementById("spinner").style.display = "none";
+    // document.getElementById("spinner").style.display = "none";
     document.getElementById("message").innerText = err.toString();
 });
